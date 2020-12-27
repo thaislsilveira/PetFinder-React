7ca400caad121066e8e088eccd2f0fb9321d1361
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-12-27 09:33:25 -0300

    add CreatePets

---
+++ petfinder-web/src/components/Sidebar/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Container = styled.aside`
+  position: fixed;
+  height: 100%;
+  padding: 32px 24px;
+  background: linear-gradient(329.54deg, #15b6d6 0%, #15d6d6 100%);
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  img {
+    width: 48px;
+  }
+
+  footer a,
+  footer button {
+    width: 48px;
+    height: 48px;
+
+    border: 0;
+
+    background: #12afcb;
+    border-radius: 16px;
+
+    cursor: pointer;
+
+    transition: background-color 0.2s;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  footer a:hover,
+  button:hover {
+    background: #17d6eb;
+  }
+`;
